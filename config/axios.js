bda966b9f85489cd8f634b6df80aae0c94c00b87
adiynil <aadiynil@gmail.com>
2022-01-10 11:00:27 +0800

    update config/axios.js

--- config/axios.js
+++ config/axios.js
@@ -1,7 +1,14 @@
+/*
+ * @Author: adiynil
+ * @Date: 2021-12-28 13:46:08
+ * @LastEditors: adiynil
+ * @LastEditTime: 2021-12-28 15:52:20
+ * @Description:
+ */
 export default {
-	baseURL: "http://localhost:5000",
-	timeout: 5000, // timeout/ms
-	withCredentials: false, // cookie
-	headers: { "Content-Type": "application/json;charset=UTF-8" },
-	responseType: "json",
-};
+  baseURL: '/api',
+  // timeout: 5000, // timeout/ms
+  withCredentials: false, // cookie
+  headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+  responseType: 'json'
+}
--- vue.config.js
+++ vue.config.js
@@ -1,40 +1,45 @@
+/*
+ * @Author: adiynil
+ * @Date: 2021-12-28 13:46:09
+ * @LastEditors: adiynil
+ * @LastEditTime: 2021-12-28 15:55:54
+ * @Description:
+ */
 module.exports = {
-	// 部署路径  / => http://my-app.com/  /myapp => http://my-app.com/myapp
-	publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
-	// 生成的生产环境构建文件的目录
-	outputDir: "dist",
-	// 放置生成的静态资源 (js、css、img、fonts) 的目录 (相对于 outputDir)
-	assetsDir: "static",
-	// 指定生成的 index.html 的输出路径 (相对于 outputDir)
-	indexPath: "index.html",
-	// 是否在生成的静态资源文件名中包含 hash 以便更好的控制缓存
-	filenameHashing: true,
-	// 是否使用包含运行时编译器的 Vue 构建版本
-	runtimeCompiler: false,
-	// 是否生成source map
-	productionSourceMap: false,
-	// css
-	css: {
-		sourceMap: false,
-	},
-	// 开发环境
-	devServer: {
-		// host: "localhost",
-		// port: "8080",
-		// 将任何未知请求 (没有匹配到静态文件的请求) 代理到目标主机
-		// proxy: "http://localhost:5000",
-		//
-		// or
-		//
-		// proxy: {
-		//   "/api": {
-		//     target: "http://localhost:5000",
-		//     changeOrigin: true,
-		//     ws: true,
-		//     pathRewrite: {
-		//       "^/api": "",
-		//     },
-		//   },
-		// },
-	},
-};
+  // 部署路径  / => http://my-app.com/  /myapp => http://my-app.com/myapp
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  // 生成的生产环境构建文件的目录
+  outputDir: 'dist',
+  // 放置生成的静态资源 (js、css、img、fonts) 的目录 (相对于 outputDir)
+  assetsDir: 'static',
+  // 指定生成的 index.html 的输出路径 (相对于 outputDir)
+  indexPath: 'index.html',
+  // 是否在生成的静态资源文件名中包含 hash 以便更好的控制缓存
+  filenameHashing: true,
+  // 是否使用包含运行时编译器的 Vue 构建版本
+  runtimeCompiler: false,
+  // 是否生成source map
+  productionSourceMap: false,
+  // css
+  css: {
+    sourceMap: false
+  },
+  // 开发环境
+  devServer: {
+    // host: "localhost",
+    // port: "8080",
+    // 将任何未知请求 (没有匹配到静态文件的请求) 代理到目标主机
+    // proxy: "http://localhost:5000",
+    //
+    // or
+    //
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        // target: 'https://wcsjyj_admin.huachengyun.cn/ptad',
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' }
+      }
+    }
+  }
+}
